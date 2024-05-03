6091c3da0ff99368c037a45fc226c5abf329d90c
CaiquedoCoutoRicardo <tad-caiquericardo@ucpparana.edu.br>
2024-05-02 22:12:19 -0300

    a

---
+++ exemplo.js
@@ -0,0 +1,18 @@
+var valor = 20;
+if ( valor > 100 )
+    console.log("Valor maior que 100");
+else
+   console.log("Valor menor que 100");
+
+var preco = 18;
+   if (preco < 10) {
+    console.log(teste = "Valor menor que 10");
+  } else if (preco < 20) {
+    console.log(teste = "valor entre 10 e 20");
+  } else {
+    console.log(teste = "maior de 20");
+  }
+  
+  for (let i = 0; i < cars.length; i++) {
+    text += cars[i] + "<br>";
+  }
